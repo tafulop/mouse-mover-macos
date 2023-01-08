3f8ffcd137afe420e65e2f7b4b58718d50ed86a3
Tamas <tamas.flp@gmail.com>
2023-01-08 18:30:27 +0100

    Add hotkey handling

--- mouse_mover.py
+++ mouse_mover.py
@@ -2,12 +2,13 @@ import macmouse
 import argparse
 
 parser = argparse.ArgumentParser(
-                    prog = 'Mouse Mover for MacOS',
-                    description = 'This script moves the mouse cursor to a user-specified absolute path.')
+    prog="Mouse Mover for MacOS",
+    description="This script moves the mouse cursor to a user-specified absolute path.",
+)
 
 
-parser.add_argument('-x', required=True)
-parser.add_argument('-y', required=True)
+parser.add_argument("-x", required=True)
+parser.add_argument("-y", required=True)
 
 args = parser.parse_args()
 
---
+++ mouse_mover_with_hotkey.py
@@ -0,0 +1,20 @@
+import macmouse
+from pynput import keyboard
+
+def move_to_position_1():
+    print("Moving to position 1.")
+    macmouse.move(x=100, y=100)
+
+
+def move_to_position_2():
+    print("Moving to position 2.")
+    macmouse.move(x=500, y=500)
+
+
+with keyboard.GlobalHotKeys(
+    {
+     "<ctrl>+<alt>+h": move_to_position_1,
+     "<ctrl>+<alt>+j": move_to_position_2
+    }
+) as h:
+    h.join()
